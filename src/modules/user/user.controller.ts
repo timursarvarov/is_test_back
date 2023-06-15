1507a7cb6577cf1688418edd7be3754555810a12
timursarvarov <53015375+timursarvarov@users.noreply.github.com>
2023-06-15 09:57:57 +0300

    Feat Add Delete User endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,78 +1,78 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpStatus,
-  NotFoundException,
-  Param,
-  Patch,
-  Post,
-  Query,
-  Res,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpStatus,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+    Query,
+    Res,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('api/users')
 export class UserController {
-  constructor(private UserService: UserService) {}
+    constructor(private UserService: UserService) { }
 
-  // add a User
-  @Post('/create')
-  async addUser(@Res() res, @Body() createUserDTO: CreateUserDTO) {
-    const User = await this.UserService.addUser(createUserDTO);
-    return res.status(HttpStatus.OK).json({
-      message: 'User has been created successfully',
-      User,
-    });
-  }
+    // add a User
+    @Post('/create')
+    async addUser(@Res() res, @Body() createUserDTO: CreateUserDTO) {
+        const User = await this.UserService.addUser(createUserDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'User has been created successfully',
+            User,
+        });
+    }
 
-  // Retrieve Users list
-  @Get()
-  async getAllUser(@Res() res) {
-    const Users = await this.UserService.getAllUser();
-    return res.status(HttpStatus.OK).json({
-      results: Users,
-    });
-  }
+    // Retrieve Users list
+    @Get()
+    async getAllUser(@Res() res) {
+        const Users = await this.UserService.getAllUser();
+        return res.status(HttpStatus.OK).json({
+            results: Users,
+        });
+    }
 
-  // Fetch a particular User using ID
-@Get('/user/:email')
-  async getUser(@Res() res, @Param('email') email) {
-    const user = await this.UserService.getUser(email);
-    if (!user) throw new NotFoundException('User does not exist!');
-    return res.status(HttpStatus.OK).json({
-      results: [user],
-    });
-  }
+    // Fetch a particular User using ID
+    @Get('/user/:email')
+    async getUser(@Res() res, @Param('email') email) {
+        const user = await this.UserService.getUser(email);
+        if (!user) throw new NotFoundException('User does not exist!');
+        return res.status(HttpStatus.OK).json({
+            results: [user],
+        });
+    }
 
-  // Update a User's details
-  @Patch('')
-  async updateUser(
-    @Res() res,
-    @Query('email') email,
-    @Body() createUserDTO: CreateUserDTO,
-  ) {
-    const user = await this.UserService.updateOrUserByEmailOrCreate(
-      email,
-      createUserDTO,
-    );
-    if (!user) throw new NotFoundException('User does not exist!');
-    return res.status(HttpStatus.OK).json({
-      message: 'User has been successfully updated',
-      user,
-    });
-  }
+    // Update a User's details
+    @Patch('')
+    async updateUser(
+        @Res() res,
+        @Query('email') email,
+        @Body() createUserDTO: CreateUserDTO,
+    ) {
+        const user = await this.UserService.updateOrUserByEmailOrCreate(
+            email,
+            createUserDTO,
+        );
+        if (!user) throw new NotFoundException('User does not exist!');
+        return res.status(HttpStatus.OK).json({
+            message: 'User has been successfully updated',
+            user,
+        });
+    }
 
-  // Delete a User
-  @Delete('/delete')
-  async deleteUser(@Res() res, @Query('userID') userID) {
-    const User = await this.UserService.deleteUser(userID);
-    if (!User) throw new NotFoundException('User does not exist');
-    return res.status(HttpStatus.OK).json({
-      message: 'User has been deleted',
-      User,
-    });
-  }
+    // Delete a User
+    @Delete(':id')
+    async deleteUser(@Res() res, @Param('id') id) {
+        const user = await this.UserService.deleteUser(id);
+        if (!user) throw new NotFoundException('User does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'User has been deleted',
+            user,
+        });
+    }
 }
